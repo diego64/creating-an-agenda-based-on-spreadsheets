d1b2b1b8426000896087ea2d7e8f2d8c121d2e77
Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>
2024-10-30 16:58:35 -0300

    chore: adjustment in the event description

--- mobilizer/createMobilizingEvent.js
+++ mobilizer/createMobilizingEvent.js
@@ -19,8 +19,10 @@ function criarEventoMobilizador() {
     if (linha[0] && linha[1]) {
       var data = linha[0]; // DATA
       var horario = linha[1]; // HORÁRIO
+      var evento = linha[2]; // EVENTO (coluna C)
       var agendamento = linha[8]; // AGENDAMENTO
       var responsavel = linha[9]; // RESPONSÁVEL PELO AGENDAMENTO
+      var telefone = linha[10]; // TELEFONE
       var status = linha[13]; // STATUS
 
       if (typeof horario === "string" && horario.includes(":")) {
@@ -33,42 +35,35 @@ function criarEventoMobilizador() {
           var dataFormatada = Utilities.formatDate(data, Session.getScriptTimeZone(), "yyyy-MM-dd");
           var dataEvento = new Date(dataFormatada + "T" + horarioString + ":00");
 
-          // Ignorar eventos para datas passadas
           if (dataEvento < new Date()) continue;
 
-          // Definir fim do evento
           var fimEvento = new Date(dataEvento);
           fimEvento.setMinutes(fimEvento.getMinutes() + 30);
 
-          // Procurar eventos existentes na agenda
           var eventosExistentes = agenda.getEvents(dataEvento, fimEvento);
           var eventoExistente = eventosExistentes.find(evento => 
             evento.getTitle().includes(responsavel) && evento.getStartTime().getTime() === dataEvento.getTime()
           );
 
-          // Definir título do evento com base no status
           var tituloEvento = status === "Cancelado"
-            ? "Cancelado | " + responsavel // Título para evento cancelado
-            : agendamento + " | " + responsavel; // Título para evento confirmado
+            ? "Cancelado | " + responsavel
+            : agendamento + " | " + responsavel;
 
           if (eventoExistente) {
-            // Verifica se o status atual do evento é diferente do status na linha
             var eventoStatusAtual = eventoExistente.getTitle().includes("Cancelado") ? "Cancelado" : "Confirmado";
 
             if (status !== eventoStatusAtual) {
               Logger.log("Mudança de status detectada. Excluindo o evento: " + eventoExistente.getTitle());
-              // Excluir o evento existente
               eventoExistente.deleteEvent();
             } else {
               Logger.log("Evento já existe e está atualizado: " + eventoExistente.getTitle());
-              continue; // Não criar um novo evento se já existe e está atualizado
+              continue;
             }
           }
 
-          // Criar um novo evento na agenda
           var descricaoEvento = status === "Cancelado"
-            ? "Evento Cancelado" // Apenas "Evento Cancelado" se for cancelado
-            : `Evento: ${agendamento}\nResponsável: ${responsavel}\nHorário: ${horarioString}`; // Descrição completa se confirmado
+            ? "Evento Cancelado"
+            : `Evento: ${evento}\nTelefone: ${telefone}\nHorário: ${horarioString}`;
 
           var criarEvento = agenda.createEvent(tituloEvento, dataEvento, fimEvento, {
             location: linha[3], // IGREJA/LOCAL
